d0d3037155ed91c246f4739c909a73d014251399
idamakela <idamakela98@gmail.com>
2023-02-24 14:47:26 +0100

    added icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { FaDice20 } from 'react-icons/fa' //<FaDice20 /> to use in return html
 import Header from './Header.jsx'
 import './App.css'
 
