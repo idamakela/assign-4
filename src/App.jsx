2b417ff53bab232a9d5d007ed9d0d3582333a50d
idamakela <idamakela98@gmail.com>
2023-02-24 16:53:20 +0100

    started element design for main

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,32 @@ function App() {
     const [object, setObject] = useState([]);
 
     useEffect(() => {
-        fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects')
-        
-    });
+        fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/67870')
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                console.log(data)
+            })
+    }, []);
 
     return (
         <>
         <Header title={'Title'} subtitle={'Subtitle'} />
 
         <main>
-            main element
+            <div className="objectContainer">
+                <div className="imgContainer">
+                    <img src="./" alt="pic" />
+                </div>
+                <div className="textContainer">
+                    <h3 className="artist"></h3>
+                    <h2 className="artPiece"></h2>
+                    <p className="desc"></p>
+                    <button className="link">Link</button>
+                </div>
+            </div>
+
         </main>
 
         <Footer />
