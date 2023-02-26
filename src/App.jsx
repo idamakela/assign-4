e842c0e246ebcdb7ce8e9e81a98ccf9edfad7aed
idamakela <idamakela98@gmail.com>
2023-02-26 18:34:56 +0100

    stuck on second fetch

--- src/App.jsx
+++ src/App.jsx
@@ -6,40 +6,47 @@ import Object from './Object.jsx';
 function App() {
     // API info: https://metmuseum.github.io/
     const API = 'https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=19/';
-    const API_OBJECT = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/260067'
+    const API_OBJECT = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/'
     
     //first fetch API then randomise a number and feth the random number (aka index in the data) obejctId, then fetch the objects 
-
-
-    const apiObject = (arr) => {
-        return Math.floor(Math.random() * arr.length)
-    }
-    const [objects, setObject] = useState([]);
+    const [currentData, setData] = useState();
+    const [object, setObjects] = useState([]);
 
     useEffect(() => {
-        fetch(API_OBJECT)
+        fetch(API)
             .then((response) => {
                 return response.json()
             })
             .then((data) => {
+                const apiObject = (arr) => {
+                    return Math.floor(Math.random() * arr.length)
+                }
+                setData(data.objectIDs[apiObject(data.objectIDs)])
                 console.log(data)
+                console.log(currentData);
                 
-                setObject(data)
+
+                //fetch object 
+                // fetch(API_OBJECT + currentData)
+                // .then((response) => response.json())
+                // .then((data) => {
+                //     console.log(data);
+                //     setObjects(data);
+                // })
+                // .catch((error) => console.error(error))
             })
+            .catch((error) => console.error(error))
     }, []);
+
+    console.log(currentData);
+    // console.log(API_OBJECT + currentData)
 
     return (
         <>
         <Header title={'Title'} subtitle={'Subtitle'} />
 
         <main> 
-            {/* the below should return ALL, it should only return 10 objects at a time
-            {
-                objects.map((object) => { */}
-                    <Object key={data.objectID} {...data} />
-                {/* })
-
-            } */}
+            <Object key={currentData} {...currentData}></Object>
         </main>
 
         <Footer />
--- src/Object.jsx
+++ src/Object.jsx
@@ -1,27 +1,20 @@
-export default ({object: artistDisplayName, medium, objectDate, title, artistDisplayBio}) => {   
+export default ({ title: primaryImage, artistDisplayName, title, objectDate, medium, objectURL, artistWikidata_URL }) => {   
     return (
         <div className="objectContainer">
             <div className="imgContainer">
-                <img src={primaryImg} alt="pic"/>
+                <img src={primaryImage} alt="pic" width="400px" height="400px"/>
             </div>
             <div className="textContainer">
                 <h3 className="artist">{artistDisplayName}</h3>
                 <h2 className="artPiece">{title}</h2>
 
-                <p className="desc">{`
-                        Medium: ${medium}
-                        Date: ${objectDate}
-                        Bio: ${artistDisplayBio}
-                    `}
+                <p className="desc">
+                    {`${objectDate} - ${medium}`}
                 </p>
 
-                <div className="btnContainer">
-                    <button className="link" onClick={() => {
-                        // artistDisplayBio: link to artist, open in new window
-                    }}>artist</button>
-                    <button className="link" onClick={() => {
-                        // objectURL: link to object, open in new window
-                    }}>object</button>
+                <div className="linkContainer">
+                    <a href={objectURL} target="_blank">object</a>
+                    <a href={artistWikidata_URL} target="_blank">artist</a>
                 </div>
             </div>
         </div>
